c0fc88571cbae3bad5bb35c1dbf240d2b158b799
unknown <7189053722@untrm.edu.pe>
2025-09-02 16:00:29 -0500

    clase ptractica

---
+++ PRACTICA.R
@@ -0,0 +1,48 @@
+# mi primera practica con R
+
+alt + -
+  
+x <- "hola"
+
+y <- "untrm"
+
+#f(x,y)=x+x
+paste(x, y, sep = " + ")
+
+# ayuda: f1 | fn + f1
+
+
+
+
+paste(x, y)
+?paste
+
+
+
+library(tidyverse)
+
+# pipe
+a1 <- "estudiante"
+a_1 <- "a"
+
+notas <- c(16, 17, 19, 15)
+media <- mean(notas)
+media
+
+# ctrl +shift + m => %>%
+
+m <- c(16, 17, 19, 15) %>% 
+  mean()
+m
+0
+
+
+
+
+
+
+
+
+
+
+
